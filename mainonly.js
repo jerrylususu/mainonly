e5018f45b08f19581cb5b5d728a4ab05d1a919f7
Hongzhang Shi <shz42@outlook.com>
2023-12-20 02:04:41 -0800

    use :not

--- mainonly.js
+++ mainonly.js
@@ -1,83 +1,72 @@
 // A JavaScript bookmarklet designed to isolate and highlight a specific element on a webpage, effectively hiding all other elements.
 
 javascript: (function () {
-    var selectedElement = null;
-  
-    var style = document.createElement("style");
-    style.textContent =
-      ".mainonly-outline { outline: 2px solid red; }  .mainonly-hidden { display: none; }";
-    document.head.appendChild(style);
-  
+    var selectedElement = document.body; // just in case no element is selected
+
+    const style = document.head.appendChild(document.createElement("style"));
+    style.textContent = ".mainonly { outline: 2px solid red; }";
+
+    /** @param {*} element */
     function outlineElement(element) {
-      if (selectedElement) {
-        selectedElement.classList.remove("mainonly-outline");
-      }
-      selectedElement = element;
-      selectedElement.classList.add("mainonly-outline");
+        if (element instanceof HTMLElement) { // Ignores non-HTMLElements
+            selectedElement.classList.remove("mainonly");
+            selectedElement = element;
+            selectedElement.classList.add("mainonly");
+        }
     }
-  
-    function removeOutline(element) {
-      element.classList.remove("mainonly-outline");
-    }
-  
+
+    /** @param {MouseEvent} event */
     function onMouseOver(event) {
-      outlineElement(event.target);
+        outlineElement(event.target);
     }
-  
-    function removeOtherElements(element) {
-      var allElements = document.body.getElementsByTagName("*");
-      for (var i = 0; i < allElements.length; i++) {
-        if (
-          element.contains(allElements[i]) ||
-          allElements[i].contains(element)
-        ) {
-          continue;
-        }
-        allElements[i].classList.add("mainonly-hidden");
-      }
-    }
-  
+
+
+    /** @param {MouseEvent} event */
     function onClick(event) {
-      event.preventDefault();
-      removeOtherElements(selectedElement);
-      cleanupEventListeners();
-      removeOutline(selectedElement);
+        event.preventDefault();
+        // If using :has() selector
+        style.textContent = ":not(.mainonly *, :has(.mainonly)) { display: none; }";
+        // In case :has() selector is not supported:
+        // style.textContent = ":not(.mainonly *, .mainonly-ancestor) { display: none; }";
+        // var /** @type {HTMLElement | null} */ curr = selectedElement;
+        // do { curr.classList.add("mainonly-ancestor"); } while (curr = curr.parentElement);
+        cleanupEventListeners();
     }
-  
-    function showHiddenElements(event) {
-      if (event.key === "Escape") {
-        var hiddenElements = document.querySelectorAll(".mainonly-hidden");
-        hiddenElements.forEach(function (element) {
-          element.classList.remove("mainonly-hidden");
-        });
-      }
-      document.removeEventListener("keydown", showHiddenElements, false);
+
+    /** @param {KeyboardEvent} event */
+    function onKeydown(event) {
+        if (event.key === "Escape") {
+            style.remove();
+            document.removeEventListener("keydown", onKeydown);
+            cleanupEventListeners();
+            selectedElement?.classList.remove("mainonly");
+            // In case :has() selector is not supported:
+            // for (const element of document.getElementsByClassName("mainonly-ancestor")) {
+            //     element.classList.remove("mainonly-ancestor");
+            // }
+        }
     }
-  
+
+    /** @param {WheelEvent} event */
     function onWheel(event) {
-      event.preventDefault();
-      if (event.deltaY < 0) {
-        // Scrolling up, select parent element
-        if (selectedElement.parentElement) {
-          outlineElement(selectedElement.parentElement);
+        event.preventDefault();
+        if (event.deltaY < 0) {
+            // Scrolling up, select parent element
+            outlineElement(selectedElement.parentElement);
+        } else {
+            // Scrolling down, select first child element
+            outlineElement(selectedElement.firstElementChild);
         }
-      } else {
-        // Scrolling down, select first child element
-        if (selectedElement.children.length > 0) {
-          outlineElement(selectedElement.children[0]);
-        }
-      }
     }
-  
+
     function cleanupEventListeners() {
-      document.removeEventListener("mouseover", onMouseOver, false);
-      document.removeEventListener("click", onClick, false);
-      document.removeEventListener("wheel", onWheel, { passive: false });
+        document.removeEventListener("mouseover", onMouseOver);
+        document.removeEventListener("click", onClick);
+        document.removeEventListener("wheel", onWheel);
     }
-  
-    document.addEventListener("mouseover", onMouseOver, false);
-    document.addEventListener("click", onClick, false);
+
+    document.addEventListener("mouseover", onMouseOver);
+    document.addEventListener("click", onClick);
     document.addEventListener("wheel", onWheel, { passive: false });
-    document.addEventListener("keydown", showHiddenElements, false);
-  })();
-  
+    document.addEventListener("keydown", onKeydown);
+})();
