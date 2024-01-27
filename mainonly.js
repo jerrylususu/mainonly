4ddc8a9d2b9078ba6d9e0a98545eff2e28ffbea5
Neko Null <jerrylu.mail@qq.com>
2024-01-27 21:39:43 +0800

    feat: add selection guide (#15)

--- mainonly.js
+++ mainonly.js
@@ -26,6 +26,41 @@
     const style = document.head.appendChild(document.createElement("style"));
     style.textContent = "#mainonly { outline: 2px solid red; }  .mainonly { outline: 2px solid red; }";
 
+    // selection guide overlay
+    const guideTextCn = '正在选择元素。按 <kbd>Esc</kbd> 键取消选择。向下滚动，或按下 <kbd>=</kbd>/<kbd>.</kbd> 键缩小选区。向上滚动，或按下 <kbd>-</kbd>/<kbd>,</kbd> 键扩大选区。'
+    const guideTextEn = 'Selecting element. Press <kbd>Esc</kbd> to cancel selection. Scroll down, or press <kbd>=</kbd>/<kbd>.</kbd> to shrink the selection. Scroll up, or press <kbd>-</kbd>/<kbd>,</kbd>, to expand the selection.'
+    const guide = document.body.appendChild(document.createElement("div"));
+    guide.className = "mainonly-guide";
+    guide.innerHTML = `<p>${guideTextCn}</p><p>${guideTextEn}</p>`;
+    const guideStyle = document.head.appendChild(document.createElement("style"));
+    guideStyle.textContent = `
+        .mainonly-guide {
+                position: fixed;
+                top: 0;
+                left: 50%; /* center the box horizontally */
+                transform: translate(-50%, 0); /* center the box horizontally */
+                padding: 0.5rem;
+                font-size: 1rem;
+                font-family: sans-serif;
+                text-align: center;
+                color: white;
+                background-color: rgba(0, 0, 0, 0.5);
+                border-radius: 0.5em;
+                z-index: 999999999;
+            kbd {
+                display: inline-block;
+                padding: 0.1em 0.3em;
+                font-size: 0.8em;
+                line-height: 1;
+                color: #24292e;
+                vertical-align: middle;
+                background-color: #fafbfc;
+                border: 1px solid #d1d5da;
+                border-radius: 3px;
+                box-shadow: inset 0 -1px 0 #d1d5da;
+            }
+        }`;
+
     /** @param {*} element */
     function outlineElement(element) {
         if (element instanceof HTMLElement) { // Ignores non-HTMLElements
@@ -69,17 +104,24 @@
             style.textContent = `* { visibility: hidden; } .mainonly, .mainonly * { visibility: visible; }`;
         }
         cleanupEventListeners();
+        hideGuideOverlay();
+    }
+
+    function hideGuideOverlay() {
+        guide.remove();
+        guideStyle.remove();
     }
 
     /** @param {KeyboardEvent} event */
     function onKeydown(event) {
+        // Prevent the default action
+        event.preventDefault();
         if (event.key === "Escape") {
-            // Prevent the default action of the escape key
-            event.preventDefault();
             // Recover the hidden elements
             style.remove();
             document.removeEventListener("keydown", onKeydown);
             cleanupEventListeners();
+            hideGuideOverlay();
             // Restore the selected element to its original state
             if (lastStrategy === 'id') {
                 // id
@@ -88,6 +130,14 @@
                 // class
                 selectedElement.classList.remove("mainonly");
             }
+        } else if (event.key === ',' || event.key === '-') {
+            // up, select parent element
+            console.log("up");
+            outlineElement(selectedElement.parentElement);
+        } else if (event.key === '.' || event.key === '=') {
+            // down, select first child element
+            console.log("down");
+            outlineElement(selectedElement.firstElementChild);
         }
     }
 
